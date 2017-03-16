6fd63eb920fcb0a4b2b43f55c5d553ee9c25aeca
LShelton <Logan_Shelton@hotmail.com>
2017-03-16 10:06:27 -0700

    Pushed entire project

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+/// <summary>
+/// Allows the use of the Generate button, or auto update.
+/// This also calls other scripts.
+/// </summary>
+[CustomEditor (typeof (MapGenerator))]
+
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        //if "auto Update" is enabled, it will auto update without "Generate"
+        if(DrawDefaultInspector())
+        {
+            if(mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+        //once "Generate" is pressed it will generate
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    /// <summary>
+    /// draws the noise map and allows the texture to be edited
+    /// Assigns colors, 0 - 1 value (black and white)
+    /// renders the plane.
+    /// </summary>
+    public Renderer textureRender;
+
+    public void DrawTexture(Texture2D texture)
+    {
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+}
